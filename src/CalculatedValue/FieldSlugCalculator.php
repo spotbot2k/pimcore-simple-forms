c89363f4f282ae23a2f2e2e934769647bf71473b
Alexander Schwirjow <spotbot2k@yahoo.de>
2022-02-12 08:46:39 +0000

    Begin implementing the slug calculator

--- src/CalculatedValue/FieldSlugCalculator.php
+++ src/CalculatedValue/FieldSlugCalculator.php
@@ -11,16 +11,21 @@ namespace SimpleFormsBundle\CalculatedValue;
 use Pimcore\Model\DataObject\ClassDefinition\CalculatorClassInterface;
 use Pimcore\Model\DataObject\Concrete;
 use Pimcore\Model\DataObject\Data\CalculatedValue;
+use Pimcore\Model\DataObject\Fieldcollection\Data\Input;
 
 class FieldSlugCalculator implements CalculatorClassInterface
 {
     public function compute(Concrete $object, CalculatedValue $context): string
     {
-        return 'xxx';
+        if ($object instanceof Input) {
+            return strtolower(str_replace(" ", "-", $object->getLabel()));
+        }
+
+        return '';
     }
 
     public function getCalculatedValueForEditMode(Concrete $object, CalculatedValue $context): string
     {
-        return 'xxx';
+        return $this->compute($object, $context);
     }
 }
