7ed959426c86b9b1e25001651ae27277361f76b2
Alexander Schwirjow <spotbot2k@yahoo.de>
2022-05-05 15:46:51 +0000

    Add consent field type

---
+++ src/Field/SimpleFormConsent.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Pimcore Simple Forms Bundle
+ *
+ *  @license GPLv3
+ */
+
+namespace SimpleFormsBundle\Field;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormEvent;
+
+class SimpleFormConsent
+{
+    public static function renderField(FormEvent &$event): void
+    {
+        $event->getForm()->add($event->getData()->getSlug(), CheckboxType::class, [
+            'mapped'     => false,
+            'label'      => $event->getData()->getLabel(),
+            'label_attr' => [ 'style' => 'display: none' ],
+            'help'       => $event->getData()->getHelp(),
+            'help_html'  => true,
+            'required'   => $event->getData()->getRequired(),
+        ]);
+    }
+}
