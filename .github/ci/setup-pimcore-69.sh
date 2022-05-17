0bb24d9fa03badb218c0cebb2983c9658b66539a
Alexander Schwirjow <spotbot2k@yahoo.de>
2022-05-17 09:02:57 +0000

    Fix composer requirement

--- .github/ci/setup-pimcore-69.sh
+++ .github/ci/setup-pimcore-69.sh
@@ -11,8 +11,6 @@ cp .github/ci/files-pimcore-69/extensions.php var/config/extensions.php
 
 composer require codeception/codeception:2.4.5 --no-update
 composer require pimcore/pimcore:6.9.6
-composer require spotbot2k/pimcore-simple-forms:dev-master
-
 
 php bin/console pimcore:bundle:install SimpleFormsBundle
 php bin/console pimcore:bundle:enable SimpleFormsBundle
